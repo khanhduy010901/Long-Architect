27bbb78812e0b290b8447a7c6e9df8f470f756bb
duy nguyen <khanhduydeptraivl@gmail.com>
2022-12-07 16:19:00 +0700

    init project

---
+++ LONG/css/long.js
@@ -0,0 +1,32 @@
+// var orders =[
+//     {
+//         name: "Nguyen Khanh Duy",
+//         age:21,
+//         money:3000
+//     },
+//     {
+//         name: "Nguyen Minh Phu",
+//         age:19,
+//         money:3000
+//     },
+//     {
+//         name:"Pham Dinh Nghia",
+//         age:19,
+//         money:2000
+//     },
+//     {
+//         name:"Nguyen Trong Viet Anh",
+//         age:19,
+//         money:5000
+//     }
+// ]
+
+// function getSum(orders){
+//     var sum =0;
+//     for(i =0; i<orders.length; i++){
+//         sum += orders[i].age
+//     }
+//     return sum;
+// }
+// alert(getSum(orders))
+
